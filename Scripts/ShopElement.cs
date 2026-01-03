c96fb897efd822c985bbbbd50ba809be58eb4ebc
Robin Lane <rubeculabee@gmail.com>
2026-01-02 20:37:56 -0800

    shop elements will now display their given information

--- Scripts/ShopElement.cs
+++ Scripts/ShopElement.cs
@@ -1,5 +1,6 @@
-using Godot;
 using System;
+using Godot;
+using Godot.Collections;
 
 public partial class ShopElement : PanelContainer
 {
@@ -9,4 +10,29 @@ public partial class ShopElement : PanelContainer
     string name;
     [Export]
     string description;
+
+    Dictionary<string, Label> labels;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        SetLabels();
+    }
+
+    private void SetLabels()
+    {
+        labels = new Dictionary<string, Label>
+        {
+            {"Name" , FindChild("Name") as Label},
+            {"Description", FindChild("Description") as Label},
+            {"Price", FindChild("Price") as Label},
+            {"Purchases", FindChild("Purchases") as Label}
+        };
+
+        labels["Name"].Text = name;
+        labels["Description"].Text = description;
+        labels["Price"].Text = $"{price}P";
+        labels["Purchases"].Text = "0x";
+    }
 }
