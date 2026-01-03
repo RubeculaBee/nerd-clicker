5db22f3ed58f2b700243225d861e0acd4e6f58a7
Robin Lane <rubeculabee@gmail.com>
2026-01-02 20:22:44 -0800

    layouted shop

---
+++ Scripts/ShopElement.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class ShopElement : PanelContainer
+{
+    [Export]
+    int price;
+    [Export]
+    string name;
+    [Export]
+    string description;
+}
